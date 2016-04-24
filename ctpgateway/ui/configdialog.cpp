6976f7af0fd434bdad5c55e05033747240fe2bed
sunwangme <sunwangme@gmail.com>
2016-04-24 20:52:25 +0800

    update

--- ctpgateway/ui/configdialog.cpp
+++ ctpgateway/ui/configdialog.cpp
@@ -21,8 +21,25 @@ void ConfigDialog::save()
     Profile* profile = g_sm->profile();
     profile->put("userId", ui->userId->text());
     profile->put("brokerId", ui->brokerId->text());
-    profile->put("frontMd", ui->frontMd->text());
-    profile->put("frontTd", ui->frontTd->text());
+
+    // 解决ctp网址缺tcp://或者大写导致的crash=
+    QString frontMd = ui->frontMd->text();
+    if(!frontMd.trimmed().toLower().startsWith("tcp://")){
+        frontMd = QString("tcp://") + frontMd.trimmed().toLower();
+    }else{
+        frontMd = frontMd.trimmed().toLower();
+    }
+    profile->put("frontMd", frontMd);
+
+    // 解决ctp网址缺tcp://或者大写导致的crash=
+    QString frontTd = ui->frontTd->text();
+    if(!frontTd.trimmed().toLower().startsWith("tcp://")){
+        frontTd = QString("tcp://") + frontTd.trimmed().toLower();
+    }else{
+        frontTd = frontTd.trimmed().toLower();
+    }
+    profile->put("frontTd", frontTd);
+
     profile->put("idPrefixList", ui->idPrefixList->text());
     profile->commit();
 }
@@ -30,9 +47,9 @@ void ConfigDialog::save()
 void ConfigDialog::load()
 {
     Profile* profile = g_sm->profile();
-    ui->userId->setText(profile->get("userId", "").toString());
-    ui->brokerId->setText(profile->get("brokerId", "").toString());
-    ui->frontMd->setText(profile->get("frontMd", "").toString());
-    ui->frontTd->setText(profile->get("frontTd", "").toString());
+    ui->userId->setText(profile->get("userId", "666666").toString());
+    ui->brokerId->setText(profile->get("brokerId", "9999").toString());
+    ui->frontMd->setText(profile->get("frontMd", "tcp://218.202.237.33:10012").toString());
+    ui->frontTd->setText(profile->get("frontTd", "tcp://218.202.237.33:10002").toString());
     ui->idPrefixList->setText(profile->get("idPrefixList", "if;ih;ic;sr;rb;pp").toString());
 }
